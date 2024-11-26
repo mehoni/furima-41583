92fbcb30bdd728d6e947a5f730ecb273b1dece4e
mehoni <takuya.himeno@gmail.com>
2024-11-27 06:10:35 +0900

    createアクションをコントローラーに定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -6,4 +6,15 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
+
+  def create
+    Item.create(Item_params)
+    redirect_to '/'
+  end
+
+  private
+
+  def Item_params
+    params.require(:item).permit(:name, :image, :text)
+  end
 end
