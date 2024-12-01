71361d027227f70420e532e5d34fa8b993827823
mehoni <takuya.himeno@gmail.com>
2024-12-01 15:29:40 +0900

    ログイン時のみ出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
     @items = Item.all
   end
@@ -8,13 +9,18 @@ class ItemsController < ApplicationController
   end
 
   def create
-    Item.create(item_params)
-    redirect_to '/'
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to item_path(@item), notice: '商品が出品されました。'
+    else
+      render :new
+    end
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:name, :image, :text)
+    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_cost_id,
+                                 :shipping_origin_id, :shipping_date_estimate_id, :image)
   end
 end
