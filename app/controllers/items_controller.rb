08919e9523745cc98cfdc8d41e63563ca23eb94e
mehoni <takuya.himeno@gmail.com>
2024-12-01 18:29:30 +0900

    出品完了・トップページ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,9 +9,9 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
+    @item = Item.new(item_params.merge(user_id: current_user.id))
     if @item.save
-      redirect_to item_path(@item), notice: '商品を出品しました！'
+      redirect_to items_path(@item), notice: '商品を出品しました！'
     else
       render :new
     end
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :description, :price, :category_id, :condition_id, :shipping_cost_id,
-                                 :shipping_origin_id, :shipping_date_estimate_id, :image)
+    params.require(:item).permit(:image, :title, :description, :category_id, :condition_id, :shipping_cost_id,
+                                 :shipping_origin_id, :shipping_date_estimate_id, :price, :user_id)
   end
 end
