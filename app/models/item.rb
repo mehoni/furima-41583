9dbd23eeb5254256ec631e44df73c0fe728b3215
mehoni <takuya.himeno@gmail.com>
2024-12-01 15:23:09 +0900

    ActiveHashのカテゴリー実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,24 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :shipping_cost
+  belongs_to_active_hash :shipping_origin
+  belongs_to_active_hash :shipping_date_estimate
+
   belongs_to :user
   has_one :purchase
   has_one_attached :image
 
   validates :image, presence: true
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_cost_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_origin_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :shipping_date_estimate_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, format: { with: /\A[0-9]+\z/, message: 'は半角数字のみで入力してください' }
 end
