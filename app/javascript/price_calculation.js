2e2d971e20de91e1180dd4248f7af61323e17493
mehoni <takuya.himeno@gmail.com>
2024-12-08 17:41:29 +0900

    price_calculation.jsの修正_2回目

--- app/javascript/price_calculation.js
+++ app/javascript/price_calculation.js
@@ -12,12 +12,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const price = parseFloat(priceInput.value); // 入力された価格
 
     if (!isNaN(price) && price >= 300 && price <= 9999999) {
-      const tax = price * 0.1; // 販売手数料（10%）
-      const profit = price - tax; // 販売利益
 
-      // Math.floorを使って小数点以下を切り捨てる
-      taxPriceElement.textContent = Math.floor(tax); // 小数点以下を切り捨て
-      profitElement.textContent = Math.floor(profit); // 小数点以下を切り捨て
+      const tax = Math.floor(price * 0.1); // 販売手数料（10%）
+      const profit = Math.floor(price - tax); // 販売利益
+
+      taxPriceElement.textContent = tax; // 小数点以下を切り捨てた販売手数料を表示
+      profitElement.textContent = profit; // 小数点以下を切り捨てた販売利益を表示
     } else {
       // 入力価格が範囲外の場合、手数料と利益は非表示にする
       taxPriceElement.textContent = '';
