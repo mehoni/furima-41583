fe3aaf063c22454ec6fc24253f66e70de3b1290a
mehoni <takuya.himeno@gmail.com>
2025-02-16 17:47:37 +0900

    売却済み商品の編集不可

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -25,6 +25,11 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+
+    return unless @item.user != current_user || @item.sold_out?
+
+    redirect_to root_path, alert: 'この商品は編集できません。'
   end
 
   def update
